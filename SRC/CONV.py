bd62f06fa2b98d030b0363848b4de748ec20713c
Arkadiusz Zarzycki <zarzycki.arek@gmail.com>
2024-07-07 12:16:53 +0200

    UPDATE

--- SRC/CONV.py
+++ SRC/CONV.py
@@ -1,7 +1,7 @@
 import fitz  # PyMuPDF
 
 cleaner = ""
-myfont = "C:/Windows/Fonts/arial.ttf"
+#myfont = "C:/Windows/Fonts/arial.ttf"
 starytekst = input("\nPodaj stary tekst: ")
 nowytekst = input("\nPodaj nowy tekst: ")
 
@@ -17,12 +17,24 @@ def replace_text_in_pdf(input_pdf_path, output_pdf_path, search_text, replace_te
         
         # Zamień każdą znalezioną instancję
         for inst in text_instances:
-            page.add_redact_annot(inst, cleaner, fontname="helv", fontsize=8.5)
+            bbox = fitz.Rect(inst)
+            font_size = page.get_text("dict", clip=bbox)["blocks"][0]["lines"][0]["spans"][0]["size"]
+            font_name = page.get_text("dict", clip=bbox)["blocks"][0]["lines"][0]["spans"][0]["font"]
+            print(f"Rozmiar czcionki: {font_size}")
+            print(f"Rozmiar czcionki: {font_name}")
+            page.add_redact_annot(inst, cleaner, fontname="helv", fontsize=font_size)
             x1, y1, xx1, yy1 = inst
+            if font_name == "ArialRegular":
+                myfont = "C:/Windows/Fonts/arial.ttf"
+                y1 = y1-1
+            if font_name == "ConsolasRegular":
+                myfont = "C:/Windows/Fonts/consola.ttf"
+            if font_name == "ConsolasBold":
+                myfont = "C:/Windows/Fonts/consolab.ttf"
             page.apply_redactions(images=fitz.PDF_REDACT_IMAGE_NONE)  # Nie zmieniaj obrazów
             page.insert_font(fontname="F0", fontfile=myfont)
-            rect = fitz.Rect(x1, y1-1, 300, 200)  # Prostokąt
-            page.insert_textbox(rect, nowytekst, fontname="F0", fontsize=8.519999504089355)
+            rect = fitz.Rect(x1, y1, 300, 200)  # Prostokąt
+            page.insert_textbox(rect, nowytekst, fontname="F0", fontsize=font_size)
     # Zapisz zmieniony plik PDF
     document.save(output_pdf_path, garbage=3, deflate=True)
 

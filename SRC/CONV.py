c3b15b5c9ee567ae3b8954ba62a1bc99b7de1247
Arkadiusz Zarzycki <zarzycki.arek@gmail.com>
2024-07-07 19:34:20 +0200

    UPDATE

--- SRC/CONV.py
+++ SRC/CONV.py
@@ -1,9 +1,15 @@
 import fitz  # PyMuPDF
+from tkinter import *
+from tkinter import filedialog
+
+root = Tk()
+root.withdraw()
 
 cleaner = ""
-#myfont = "C:/Windows/Fonts/arial.ttf"
 starytekst = input("\nPodaj stary tekst: ")
 nowytekst = input("\nPodaj nowy tekst: ")
+input_pdf_path = filedialog.askopenfilename()
+output_pdf_path = input_pdf_path[:-4] + "_N" + input_pdf_path[-4:]
 
 def replace_text_in_pdf(input_pdf_path, output_pdf_path, search_text, replace_text):
     # Otwórz istniejący plik PDF
@@ -20,8 +26,8 @@ def replace_text_in_pdf(input_pdf_path, output_pdf_path, search_text, replace_te
             bbox = fitz.Rect(inst)
             font_size = page.get_text("dict", clip=bbox)["blocks"][0]["lines"][0]["spans"][0]["size"]
             font_name = page.get_text("dict", clip=bbox)["blocks"][0]["lines"][0]["spans"][0]["font"]
-            print(f"Rozmiar czcionki: {font_size}")
-            print(f"Rozmiar czcionki: {font_name}")
+            #print(f"Rozmiar czcionki: {font_size}")
+            #print(f"Rozmiar czcionki: {font_name}")
             page.add_redact_annot(inst, cleaner, fontname="helv", fontsize=font_size)
             x1, y1, xx1, yy1 = inst
             if font_name == "ArialRegular":
@@ -37,10 +43,6 @@ def replace_text_in_pdf(input_pdf_path, output_pdf_path, search_text, replace_te
             page.insert_textbox(rect, nowytekst, fontname="F0", fontsize=font_size)
     # Zapisz zmieniony plik PDF
     document.save(output_pdf_path, garbage=3, deflate=True)
-
-# Ścieżki do plików PDF
-input_pdf_path = 'C:/CONV/OLD.pdf'
-output_pdf_path = 'C:/CONV/NEW.pdf'
 
 # Zamiana tekstu
 replace_text_in_pdf(input_pdf_path, output_pdf_path, starytekst, cleaner)
